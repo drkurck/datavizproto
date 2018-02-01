4c752b9f1a2bef78fa550a7b9213299512042127
Maxence Vannier <maxence.vannier@gmail.com>
2018-02-01 12:08:55 +0100

    create chart from database field applyed only to one table but need to be changed

--- controller/datahandler.js
+++ controller/datahandler.js
@@ -1,30 +1,30 @@
 var fs = require('fs');
 
 module.exports.controller = function(app) {
-
+    
     // TODO: put route in global file
     app.get('/datajson', function(req, res) {
         var global = load();
-
+        
         var dataPR = prepareData(productRatio(global.order), 'pie');
         var dataPS = prepareData(productSales(global.order), 'bar');
-
+        
         res.render('pages/datajson', {
             titlePR : 'Quantité produit vendu',
             dataPR  : dataPR,
             titlePS : 'Vente des produit',
             dataPS  : dataPS
         });
-  });
+    });
 }
 
 function prepareData(dataIn, chart) {
     switch (chart) {
         case 'pie':
-            dataOut = basicPrepare(dataIn)
-            break;
+        dataOut = basicPrepare(dataIn)
+        break;
         default:
-            dataOut = basicPrepare(dataIn)
+        dataOut = basicPrepare(dataIn)
     }
     console.log(dataOut);
     return dataOut;
@@ -41,78 +41,78 @@ function basicPrepare(data) {
     return dataOut
 }
 function productRatio(order) {
-  var result = new Array();
-  order.forEach(function(data, index) {
-    for (var i = 0; i < data.newObject.Dbc_Linecart.length; i++) {
-
-        if (result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] == undefined) {
-            result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] = parseInt(data.newObject.Dbc_Linecart[i].Fixedproduct.quantity);
-        } else {
-            result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] += parseInt(data.newObject.Dbc_Linecart[i].Fixedproduct.quantity);
+    var result = new Array();
+    order.forEach(function(data, index) {
+        for (var i = 0; i < data.newObject.Dbc_Linecart.length; i++) {
+            
+            if (result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] == undefined) {
+                result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] = parseInt(data.newObject.Dbc_Linecart[i].Fixedproduct.quantity);
+            } else {
+                result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] += parseInt(data.newObject.Dbc_Linecart[i].Fixedproduct.quantity);
+            }
         }
-    }
-  })
-  return result;
+    })
+    return result;
 }
 
 function productSales(order) {
-  var result = new Array();
-  order.forEach(function(data, index) {
-    for (var i = 0; i < data.newObject.Dbc_Linecart.length; i++) {
-
-        if (result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] == undefined) {
-            result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] = parseInt(data.newObject.Dbc_Linecart[i].price_ttc);
-        } else {
-            result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] += parseInt(data.newObject.Dbc_Linecart[i].price_ttc);
+    var result = new Array();
+    order.forEach(function(data, index) {
+        for (var i = 0; i < data.newObject.Dbc_Linecart.length; i++) {
+            
+            if (result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] == undefined) {
+                result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] = parseInt(data.newObject.Dbc_Linecart[i].price_ttc);
+            } else {
+                result[data.newObject.Dbc_Linecart[i].Fixedproduct.name] += parseInt(data.newObject.Dbc_Linecart[i].price_ttc);
+            }
         }
-    }
-  })
-  return result;
+    })
+    return result;
 }
 
 function load() {
-  var file = 'traces.json';
-
-  var post = new Array();
-  var order = new Array();
-  var linecart = new Array();
-  var production = new Array();
-  var product = new Array();
-
-  var text = fs.readFileSync('traces.json', 'utf8')
-  data = text.split(/\r\n/)
-
-  data.forEach(function(line, index) {
-    if (line.length > 0) {
-      line = JSON.parse(line)
-
-      switch (line.model_to_join) {
-        case 'Post':
-          post.push(line)
-          break;
-        case 'Dbc_Order':
-          order.push(line)
-          break;
-        case 'Dbc_Linecart':
-          linecart.push(line)
-          break;
-        case 'Dbc_Production':
-          production.push(line)
-          break;
-        case 'Fixedproduct':
-          product.push(line)
-          break;
-        default:
-
-      }
-    }
-  });
-  var global = new Array();
-  global.order = order;
-  global.post = post;
-  global.linecart = linecart;
-  global.production = production;
-  global.product = product;
-
-  return global;
+    var file = 'traces.json';
+    
+    var post = new Array();
+    var order = new Array();
+    var linecart = new Array();
+    var production = new Array();
+    var product = new Array();
+    
+    var text = fs.readFileSync('traces.json', 'utf8')
+    data = text.split(/\r\n/)
+    
+    data.forEach(function(line, index) {
+        if (line.length > 0) {
+            line = JSON.parse(line)
+            
+            switch (line.model_to_join) {
+                case 'Post':
+                post.push(line)
+                break;
+                case 'Dbc_Order':
+                order.push(line)
+                break;
+                case 'Dbc_Linecart':
+                linecart.push(line)
+                break;
+                case 'Dbc_Production':
+                production.push(line)
+                break;
+                case 'Fixedproduct':
+                product.push(line)
+                break;
+                default:
+                
+            }
+        }
+    });
+    var global = new Array();
+    global.order = order;
+    global.post = post;
+    global.linecart = linecart;
+    global.production = production;
+    global.product = product;
+    
+    return global;
 }
--- controller/dbhandler.js
+++ controller/dbhandler.js
@@ -2,34 +2,63 @@ var db = require('../db')
 db.connect();
 
 module.exports.controller = function(app) {
-
-  // TODO: put route in global file
-  app.get('/datadb', function(req, res) {
-    col = getColumnName(db);
-    console.log(col);
-    res.render('pages/datadb', {
-      column: col
-    })
-
-
-  });
-}
-
-function getColumnName(db) {
-  column = Array();
-  column.name = Array();
-  column.type = Array();
-  // TODO: add aliases for user-friendly column_name
-  db.query(
-    "SELECT  COLUMN_NAME , COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = \'dbcorders\'",
-    function(err, rows, fields) {
-      if (err) throw err
-      for (var key in rows) {
-        column.push({
-          'name': rows[key]['COLUMN_NAME'],
-          'type': rows[key]['COLUMN_TYPE'],
-        })
-      }
-    })
-  return column;
-}
+    
+    // TODO: put route in global file
+    app.get('/datadb', function(req, res) {
+        column = Array();
+        column.name = Array();
+        column.type = Array();
+        // TODO: add aliases for user-friendly column_name
+        db.query(
+            "SELECT  COLUMN_NAME , COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = \'dbcorders\'",
+            function(err, rows, fields) {
+                if (err) throw err
+                for (var key in rows) {
+                    column.push({
+                        'name': rows[key]['COLUMN_NAME'],
+                        'type': rows[key]['COLUMN_TYPE'],
+                    })
+                }
+                res.render('pages/datadb', {
+                    column: column
+                })
+            })
+        });
+        
+        app.post('/datadb', function(req, res) {
+            var request = '';
+            if (req.body.operation === 'none') {
+                request = "SELECT "+req.body.arg1+" AS elmt1, "+req.body.arg2+ " AS elmt2 FROM dbcorders GROUP BY "+ req.body.arg2
+            } else {
+                request = "SELECT "+ req.body.operation +"("+req.body.arg1+") AS elmt1, "+req.body.arg2+ " AS elmt2 FROM dbcorders GROUP BY "+ req.body.arg2
+                
+            }
+            console.log(request);
+            var data = new Array();
+            var label = new Array();
+            var colors = new Array();
+            // TODO: add aliases for user-friendly column_name
+            db.query(
+                request,
+                function(err, rows, fields) {
+                    if (err) throw err
+                    for (var key in rows) {
+                        if (rows[key]['elmt2'] != null) {
+                            label.push(rows[key]['elmt2']);
+                            data.push(rows[key]['elmt1']);
+                            colors.push('#'+(Math.random()*0xFFFFFF<<0).toString(16));
+                        }
+                    }
+                    console.log(data);
+                    console.log(label);
+                    
+                    res.render('pages/datadb', {
+                        column: new Array(),
+                        chart: req.body.chart,
+                        data: data,
+                        label: label,
+                        colors: colors 
+                    });
+                })
+            })
+        }
