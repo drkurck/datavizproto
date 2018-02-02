9d292c9d3da5cf71b20aac19919210293dd80090
Maxence Vannier <maxence.vannier@.com>
2018-02-02 16:58:42 +0100

    prepare datatable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
-var bodyParser = require('body-parser'); 
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var multer = require('multer');
@@ -12,19 +12,23 @@ var app = express()
 
 
 
- 
-
 // some environment variables
 app.set('port', process.env.PORT || 8000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
-app.use(session({ resave: true, saveUninitialized: true, secret: 'uwotm8' }));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'uwotm8'
+}));
 // parse application/json
-app.use(bodyParser.json());                        
+app.use(bodyParser.json());
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use('/chart', express.static(__dirname + '/node_modules/chart.js/dist/'));
 
@@ -34,17 +38,17 @@ app.use('/chart', express.static(__dirname + '/node_modules/chart.js/dist/'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // dynamically include routes (Controller)
-fs.readdirSync('./controller').forEach(function (file) {
-  if(file.substr(-3) == '.js') {
-      route = require('./controller/' + file);
-      route.controller(app);
+fs.readdirSync('./controller').forEach(function(file) {
+  if (file.substr(-3) == '.js') {
+    route = require('./controller/' + file);
+    route.controller(app);
   }
 });
 
-var server = http.createServer (app);
+var server = http.createServer(app);
 
-server.listen(app.get('port'), function(){
-   console.log('Express server on port ' + app.get('port'));
+server.listen(app.get('port'), function() {
+  console.log('Express server on port ' + app.get('port'));
 });
 
 
@@ -52,5 +56,5 @@ app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 // TODO: put route in global file
 app.get('/', function(req, res) {
-    res.render('pages/home');
+  res.render('pages/home');
 });
--- controller/dbhandler.js
+++ controller/dbhandler.js
@@ -2,85 +2,113 @@ var db = require('../db')
 db.connect();
 
 module.exports.controller = function(app) {
-    
-    // TODO: put route in global file
-    app.get('/datadb', function(req, res) {
-        column = Array();
-        column.name = Array();
-        column.type = Array();
-        // TODO: add aliases for user-friendly column_name
-        db.query(
-            "SELECT  COLUMN_NAME , COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = \'dbcorders\'",
-            function(err, rows, fields) {
-                if (err) throw err
-                for (var key in rows) {
-                    column.push({
-                        'name': rows[key]['COLUMN_NAME'],
-                        'type': rows[key]['COLUMN_TYPE'],
-                    })
-                }
-                res.render('pages/datadb', {
-                    column: column
-                })
-            })
-        });
-        
-    app.post('/datadb', function(req, res) {
-        var request = '';
-        if (req.body.operation === 'none') {
-            request = "SELECT "+req.body.arg1+" AS elmt1, "+req.body.arg2+ " AS elmt2 FROM dbcorders GROUP BY "+ req.body.arg2
-        } else {
-            if (req.body.arg2 == 'created') {
-                request = "SELECT "+ req.body.operation +"("+req.body.arg1+") AS elmt1, DATE_FORMAT("+req.body.arg2+", \"%d %M %Y\") AS elmt2 FROM dbcorders GROUP BY DATE_FORMAT("+req.body.arg2+", \"%d %M %Y\")"
-            } else {
-                request = "SELECT "+ req.body.operation +"("+req.body.arg1+") AS elmt1, "+req.body.arg2+ " AS elmt2 FROM dbcorders GROUP BY "+ req.body.arg2
-            }
-            
+
+  // TODO: put route in global file
+  app.get('/datadb', function(req, res) {
+    column = Array();
+    column.name = Array();
+    column.type = Array();
+    // TODO: add aliases for user-friendly column_name
+    db.query(
+      "SELECT  COLUMN_NAME , COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = \'dbcorders\'",
+      function(err, rows, fields) {
+        if (err) throw err
+        for (var key in rows) {
+          column.push({
+            'name': rows[key]['COLUMN_NAME'],
+            'type': rows[key]['COLUMN_TYPE'],
+          })
         }
-        console.log(request);
-        var data = new Array();
-        var label = new Array();
-        var colors = new Array();
-        // TODO: add aliases for user-friendly column_name
-        db.query(
-            request,
-            function(err, rows, fields) {
-                if (err) throw err
-                    for (var key in rows) {
-                        if (rows[key]['elmt2'] != null) {
-                            label.push(rows[key]['elmt2']);
-                            data.push(rows[key]['elmt1']);
-                            colors.push('#'+(Math.random()*0xFFFFFF<<0).toString(16));
-                        }
-                    }
-                
-                res.render('pages/datadb', {
-                    column: new Array(),
-                    chart: req.body.chart,
-                    data: data,
-                    label: label,
-                    colors: colors 
-                });
-            })
+        res.render('pages/datadb', {
+          column: column
         })
-    app.get('/datatable', function(req, res) {
-        column = Array();
-        column.name = Array();
-        column.type = Array();
-        // TODO: add aliases for user-friendly column_name
-        db.query(
-            "SELECT  COLUMN_NAME , COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = \'dbcorders\'",
-            function(err, rows, fields) {
-                if (err) throw err
-                for (var key in rows) {
-                    column.push({
-                        'name': rows[key]['COLUMN_NAME'],
-                        'type': rows[key]['COLUMN_TYPE'],
-                    })
-                }
-                res.render('pages/datatable', {
-                    column: column
-                })
-            })
-        });
+      })
+  });
+
+  app.post('/datadb', function(req, res) {
+    var request = '';
+    if (req.body.operation === 'none') {
+      request = "SELECT " + req.body.arg1 + " AS elmt1, " + req.body.arg2 +
+        " AS elmt2 FROM dbcorders GROUP BY " + req.body.arg2
+    } else {
+      if (req.body.arg2 == 'created') {
+        request = "SELECT " + req.body.operation + "(" + req.body.arg1 +
+          ") AS elmt1, DATE_FORMAT(" + req.body.arg2 +
+          ", \"%d %M %Y\") AS elmt2 FROM dbcorders GROUP BY DATE_FORMAT(" +
+          req.body.arg2 + ", \"%d %M %Y\")"
+      } else {
+        request = "SELECT " + req.body.operation + "(" + req.body.arg1 +
+          ") AS elmt1, " + req.body.arg2 +
+          " AS elmt2 FROM dbcorders GROUP BY " + req.body.arg2
+      }
+
     }
+    var data = new Array();
+    var label = new Array();
+    var colors = new Array();
+    // TODO: add aliases for user-friendly column_name
+    db.query(
+      request,
+      function(err, rows, fields) {
+        if (err) throw err
+        for (var key in rows) {
+          if (rows[key]['elmt2'] != null) {
+            label.push(rows[key]['elmt2']);
+            data.push(rows[key]['elmt1']);
+            colors.push('#' + (Math.random() * 0xFFFFFF << 0).toString(
+              16));
+          }
+        }
+
+        res.render('pages/datadb', {
+          column: new Array(),
+          chart: req.body.chart,
+          data: data,
+          label: label,
+          colors: colors
+        });
+      })
+  })
+  app.get('/datatable', function(req, res) {
+    column = Array();
+    column.name = Array();
+    column.type = Array();
+    // TODO: add aliases for user-friendly column_name
+    db.query(
+      "SELECT  COLUMN_NAME , COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = \'dbcorders\' AND table_schema != \'...\'",
+      function(err, rows, fields) {
+        if (err) throw err
+        for (var key in rows) {
+          column.push({
+            'name': rows[key]['COLUMN_NAME'],
+            'type': rows[key]['COLUMN_TYPE'],
+          })
+        }
+        res.render('pages/datatable', {
+          column: column
+        })
+      })
+  });
+
+  app.post('/dataTable', function(req, res) {
+    var select = '';
+    var i = 0;
+    for (elmt in req.body) {
+      select += elmt + ' AS col' + i + ', ';
+      i++;
+    }
+    select = "SELECT " + select.substring(0, select.length - 2) +
+      " FROM dbcorders";
+    db.query(
+      select,
+      function(err, rows, fields) {
+        if (err) throw err
+        for (var key in rows) {
+          console.log(rows);
+        }
+        res.render('pages/datatable', {
+          column: column
+        })
+      })
+  })
+}
