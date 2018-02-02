a3b49d2acd3e55c6a8e949e3243049cca5504bdb
Maxence Vannier <maxence.vannier@gmail.com>
2018-02-02 11:39:41 +0100

    prepare datatable views

--- controller/dbhandler.js
+++ controller/dbhandler.js
@@ -25,27 +25,27 @@ module.exports.controller = function(app) {
             })
         });
         
-        app.post('/datadb', function(req, res) {
-            var request = '';
-            if (req.body.operation === 'none') {
-                request = "SELECT "+req.body.arg1+" AS elmt1, "+req.body.arg2+ " AS elmt2 FROM dbcorders GROUP BY "+ req.body.arg2
+    app.post('/datadb', function(req, res) {
+        var request = '';
+        if (req.body.operation === 'none') {
+            request = "SELECT "+req.body.arg1+" AS elmt1, "+req.body.arg2+ " AS elmt2 FROM dbcorders GROUP BY "+ req.body.arg2
+        } else {
+            if (req.body.arg2 == 'created') {
+                request = "SELECT "+ req.body.operation +"("+req.body.arg1+") AS elmt1, DATE_FORMAT("+req.body.arg2+", \"%d %M %Y\") AS elmt2 FROM dbcorders GROUP BY DATE_FORMAT("+req.body.arg2+", \"%d %M %Y\")"
             } else {
-                if (req.body.arg2 == 'created') {
-                    request = "SELECT "+ req.body.operation +"("+req.body.arg1+") AS elmt1, DATE_FORMAT("+req.body.arg2+", \"%d %M %Y\") AS elmt2 FROM dbcorders GROUP BY DATE_FORMAT("+req.body.arg2+", \"%d %M %Y\")"
-                } else {
-                    request = "SELECT "+ req.body.operation +"("+req.body.arg1+") AS elmt1, "+req.body.arg2+ " AS elmt2 FROM dbcorders GROUP BY "+ req.body.arg2
-                }
-                
+                request = "SELECT "+ req.body.operation +"("+req.body.arg1+") AS elmt1, "+req.body.arg2+ " AS elmt2 FROM dbcorders GROUP BY "+ req.body.arg2
             }
-            console.log(request);
-            var data = new Array();
-            var label = new Array();
-            var colors = new Array();
-            // TODO: add aliases for user-friendly column_name
-            db.query(
-                request,
-                function(err, rows, fields) {
-                    if (err) throw err
+            
+        }
+        console.log(request);
+        var data = new Array();
+        var label = new Array();
+        var colors = new Array();
+        // TODO: add aliases for user-friendly column_name
+        db.query(
+            request,
+            function(err, rows, fields) {
+                if (err) throw err
                     for (var key in rows) {
                         if (rows[key]['elmt2'] != null) {
                             label.push(rows[key]['elmt2']);
@@ -53,14 +53,34 @@ module.exports.controller = function(app) {
                             colors.push('#'+(Math.random()*0xFFFFFF<<0).toString(16));
                         }
                     }
-                    
-                    res.render('pages/datadb', {
-                        column: new Array(),
-                        chart: req.body.chart,
-                        data: data,
-                        label: label,
-                        colors: colors 
-                    });
+                
+                res.render('pages/datadb', {
+                    column: new Array(),
+                    chart: req.body.chart,
+                    data: data,
+                    label: label,
+                    colors: colors 
+                });
+            })
+        })
+    app.get('/datatable', function(req, res) {
+        column = Array();
+        column.name = Array();
+        column.type = Array();
+        // TODO: add aliases for user-friendly column_name
+        db.query(
+            "SELECT  COLUMN_NAME , COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = \'dbcorders\'",
+            function(err, rows, fields) {
+                if (err) throw err
+                for (var key in rows) {
+                    column.push({
+                        'name': rows[key]['COLUMN_NAME'],
+                        'type': rows[key]['COLUMN_TYPE'],
+                    })
+                }
+                res.render('pages/datatable', {
+                    column: column
                 })
             })
-        }
+        });
+    }
